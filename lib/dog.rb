735cf50836bed596be1be30a394fd79844ed5584
infosecmentor <muhambiphares@gmail.com>
2022-08-21 12:22:34 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,9 @@
-class Person
+class Dog 
   attr_accessor :name
+  attr_reader :breed
 
-  def initialize(name)
+  def initialize(name, breed = "james")
       @name = name
+      @breed = breed
   end
 end
